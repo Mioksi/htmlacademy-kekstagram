dbf64e3d50b777d84ccb959a3cec2f25f51aa9a6
Mioksi <mio.natali@hotmail.com>
2020-03-12 15:33:27 +0300

    Добавляет фильтрацию изображений

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,21 @@
 'use strict';
 
 (function () {
+  var NEW_PICTURES_AMOUNT = 10;
+
+  var pictures = [];
+
+  var imgFilters = document.querySelector('.img-filters');
+  var imgFiltersForm = imgFilters.querySelector('.img-filters__form');
+
   var onSuccess = function (data) {
-    window.picture.render(data);
+    pictures = data;
+
+    window.picture.render(pictures);
+
+    imgFilters.classList.remove('img-filters--inactive');
+
+    imgFiltersForm.addEventListener('click', onFilterChange);
   };
 
   var onError = function (errorMessage) {
@@ -20,6 +33,55 @@
       });
     }
   };
+
+  var showDefaultPictures = function () {
+    window.picture.render(pictures);
+  };
+
+  var showRandomPictures = function () {
+    var randomPictures = pictures.slice();
+
+    window.utils.shuffleArray(randomPictures).splice(0, NEW_PICTURES_AMOUNT);
+
+    window.picture.render(randomPictures);
+  };
+
+  var showDiscussedPictures = function () {
+    var discussedPictures = pictures.slice();
+
+    discussedPictures.sort(function (first, second) {
+      return second.comments.length - first.comments.length;
+    });
+
+    window.picture.render(discussedPictures);
+  };
+
+  var filters = {
+    'filter-default': showDefaultPictures,
+    'filter-random': showRandomPictures,
+    'filter-discussed': showDiscussedPictures
+  };
+
+  var removeAllPictures = function () {
+    var picture = document.querySelectorAll('.picture');
+
+    picture.forEach(function (item) {
+      item.remove();
+    });
+  };
+
+  var toggleActiveFilter = function (evt) {
+    var activeFilter = imgFiltersForm.querySelector('.img-filters__button--active');
+
+    activeFilter.classList.remove('img-filters__button--active');
+    evt.target.classList.add('img-filters__button--active');
+  };
+
+  var onFilterChange = window.debounce(function (evt) {
+    removeAllPictures();
+    toggleActiveFilter(evt);
+    filters[evt.target.id]();
+  });
 
   window.backend.load(onSuccess, onError);
 })();
